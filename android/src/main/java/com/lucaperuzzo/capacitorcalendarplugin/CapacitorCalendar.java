ae1ff98db1e0d97f941e23a53e38f65107353479
Luca Peruzzo <luca.peruzzo@sharry.land>
2023-12-22 08:58:40 +0100

    Initial commit

---
+++ android/src/main/java/com/lucaperuzzo/capacitorcalendarplugin/CapacitorCalendar.java
@@ -0,0 +1,11 @@
+package com.lucaperuzzo.capacitorcalendarplugin;
+
+import android.util.Log;
+
+public class CapacitorCalendar {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
